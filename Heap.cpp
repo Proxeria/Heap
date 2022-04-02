fb7e47de20c46449848e8103697eacb116524c3d
Proxeria <napinpdx@me.com>
2022-04-02 13:32:51 -0700

    got rid of tree, setup for heap class

--- Heap.cpp
+++ Heap.cpp
@@ -1,31 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
-struct Node {
-  int data;
-  struct Node* left;
-  struct Node* right;
+void swap(int *x, int *y);
 
-  Node(int val) {
-    data = val;
 
-    //initialize l/r node children to null
-    left = NULL;
-    right = NULL;
-  }
+//class for min heap
+class MinHeap {
+
+public:
+  //constructor
+  MinHeap(int capacity);
+
+  //heapify a subtree with the root at a given index
+  void MinHeapify(int );
+
+  int parent(int i) { return (i-1)/2; }
+  
+    // to get index of left child of node at index i
+    int left(int i) { return (2*i + 1); }
+  
+    // to get index of right child of node at index i
+    int right(int i) { return (2*i + 2); }
+  
+    // to extract the root which is the minimum element
+    int extractMin();
+  
+    // Decreases key value of key at index i to new_val
+    void decreaseKey(int i, int new_val);
+  
+    // Returns the minimum key (key at root) from min heap
+    int getMin() { return harr[0]; }
+  
+    // Deletes a key stored at index i
+    void deleteKey(int i);
+  
+    // Inserts a new key 'k'
+    void insertKey(int k);
 };
 
 int main() {
-  //make root
-  struct Node* root = new Node(1);
-
-  //follow tree
-  root->left = new Node(2);
-  root->right = new Node(3);
-
-  root->left = new Node(4);
-
-  return 0;
+  
 }
