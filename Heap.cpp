25e2ba594590b3bbaa36606b1895214f21889d99
Proxeria <napinpdx@me.com>
2022-04-06 14:49:36 -0700

    beginning of max heap implementation

--- Heap.cpp
+++ Heap.cpp
@@ -9,17 +9,17 @@ void swap(int *x, int *y);
 
 //code is based off of example from https://www.geeksforgeeks.org/binary-heap/?ref=lbp
 //class for min heap
-class MinHeap {
+class MaxHeap {
 
   int *harr;
   int capacity;
   int heap_size;
 public:
   //constructor
-  MinHeap(int capacity);
+  MaxHeap(int capacity);
 
   //heapify a subtree with the root at a given index
-  void MinHeapify(int );
+  void MaxHeapify(int );
 
   int parent(int i) { return (i-1)/2; }
   
@@ -30,13 +30,13 @@ public:
     int right(int i) { return (2*i + 2); }
   
     // to extract the root which is the minimum element
-    int extractMin();
+    int extractMax();
   
     // Decreases key value of key at index i to new_val
     void decreaseKey(int i, int new_val);
   
     // Returns the minimum key (key at root) from min heap
-    int getMin() { return harr[0]; }
+    int getMax() { return harr[0]; }
   
     // Deletes a key stored at index i
     void deleteKey(int i);
@@ -46,92 +46,43 @@ public:
 };
 
 // Constructor: Builds a heap from a given array a[] of given size
-MinHeap::MinHeap(int cap)
+MaxHeap::MaxHeap(int cap)
 {
-    heap_size = 0;
-    capacity = cap;
-    harr = new int[cap];
+    
 }
   
 // Inserts a new key 'k'
-void MinHeap::insertKey(int k)
+void MaxHeap::insertKey(int k)
 {
-    if (heap_size == capacity)
-    {
-        cout << "\nOverflow: Could not insertKey\n";
-        return;
-    }
-  
-    // First insert the new key at the end
-    heap_size++;
-    int i = heap_size - 1;
-    harr[i] = k;
-  
-    // Fix the min heap property if it is violated
-    while (i != 0 && harr[parent(i)] > harr[i])
-    {
-       swap(&harr[i], &harr[parent(i)]);
-       i = parent(i);
-    }
+    
 }
   
 // Decreases value of key at index 'i' to new_val.  It is assumed that
 // new_val is smaller than harr[i].
-void MinHeap::decreaseKey(int i, int new_val)
+void MaxHeap::decreaseKey(int i, int new_val)
 {
-    harr[i] = new_val;
-    while (i != 0 && harr[parent(i)] > harr[i])
-    {
-       swap(&harr[i], &harr[parent(i)]);
-       i = parent(i);
-    }
+    
 }
   
 // Method to remove minimum element (or root) from min heap
-int MinHeap::extractMin()
+int MaxHeap::extractMax()
 {
-    if (heap_size <= 0)
-        return INT_MAX;
-    if (heap_size == 1)
-    {
-        heap_size--;
-        return harr[0];
-    }
-  
-    // Store the minimum value, and remove it from heap
-    int root = harr[0];
-    harr[0] = harr[heap_size-1];
-    heap_size--;
-    MinHeapify(0);
-  
-    return root;
+    
 }
   
   
 // This function deletes key at index i. It first reduced value to minus
 // infinite, then calls extractMin()
-void MinHeap::deleteKey(int i)
+void MaxHeap::deleteKey(int i)
 {
-    decreaseKey(i, INT_MIN);
-    extractMin();
+    
 }
   
 // A recursive method to heapify a subtree with the root at given index
 // This method assumes that the subtrees are already heapified
-void MinHeap::MinHeapify(int i)
+void MaxHeap::MaxHeapify(int i)
 {
-    int l = left(i);
-    int r = right(i);
-    int smallest = i;
-    if (l < heap_size && harr[l] < harr[i])
-        smallest = l;
-    if (r < heap_size && harr[r] < harr[smallest])
-        smallest = r;
-    if (smallest != i)
-    {
-        swap(&harr[i], &harr[smallest]);
-        MinHeapify(smallest);
-    }
+    
 }
   
 // A utility function to swap two elements
@@ -157,7 +108,7 @@ int main()
   cout << "File: will use the list of names provided on the .txt file included" << endl;
   cin >> input;
 
-  MinHeap h(100);
+  //MinHeap h(100);
   if (input == "Line") {
     while (true) {
       int tempIn;
@@ -168,32 +119,15 @@ int main()
       if (inputNum == -1) {
 	break;
       }
-      h.insertKey(inputNum);
+      // h.insertKey(inputNum);
     }
   }
   else if (input == "File") {
     while (std::getline(fileInput, line)) {
       inputNum = atoi(line.c_str());
-      h.insertKey(inputNum);
+      //h.insertKey(inputNum);
     }
   }
 
-  cout << h.getMin() << endl;
-
-  
-  /*
-    MinHeap h(11);
-    h.insertKey(3);
-    h.insertKey(2);
-    h.deleteKey(1);
-    h.insertKey(15);
-    h.insertKey(5);
-    h.insertKey(4);
-    h.insertKey(45);
-    cout << h.extractMin() << " ";
-    cout << h.getMin() << " ";
-    h.decreaseKey(2, 1);
-    cout << h.getMin() << endl;
-    return 0;
-  */
+  // cout << h.getMin() << endl;
 }
