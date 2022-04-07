61b5286c80a77a349520458ceaff7225644c6e3c
Proxeria <napinpdx@me.com>
2022-04-06 19:58:42 -0700

    class setup mostly done

--- Heap.cpp
+++ Heap.cpp
@@ -2,8 +2,55 @@
 #include <vector>
 #include <climits>
 #include <fstream>
+#include <assert.h>
 
 using namespace std;
+
+class Heap {
+public:
+  void insert(int);
+  int rmMax(void);
+  int getMax(void);
+  int getCount(void);
+private:
+  int arrVol = 0;
+  int intArray[100];
+  void heapify(void);
+};
+
+int Heap::getCount() {
+  return arrVol;
+}
+
+int Heap::getMax() {
+  assert(arrVol > 0);
+  return intArray[0];
+}
+
+void Heap::heapify() {
+  return;
+}
+
+void Heap::insert(int inInt) {
+  assert(arrVol <= 100);
+  intArray[arrVol] = inInt;
+  arrVol++;
+  heapify();
+
+  return;
+}
+
+int Heap::rmMax() {
+  assert(arrVol > 0);
+  int tempInt = intArray[0];
+  intArray[0] = intArray[arrVol - 1];
+  arrVol--;
+  heapify();
+  
+  return tempInt;
+}
+
+
 
 void max_heap(int *a, int m, int numofNum) {
   int j, t;
@@ -34,13 +81,15 @@ void build_maxheap(int *a, int numofNum) {
 
 // Driver program to test above functions
 int main() {
-  int numLines = 0;
+  Heap thisHeap;
   ifstream fileInput("input.txt");
   string line;
   string input;
   string tempStr;
+  int tempInt;
   int inputNum;
   vector<int> inputVect;
+  
   cout << "Commands (Case Sensitive):" << endl;
   cout << "Line: enter a list of numbers in the input line" << endl;
   cout << "File: will use the list of names provided on the .txt file included" << endl;
@@ -50,24 +99,29 @@ int main() {
     int numofNum, i;
     cout << "How many number will you enter?" << endl;
     cin >> numofNum;
-    int a[30];
+    
+    
     for (i = 1; i <= numofNum; i++) {
-      cout << "enter elements" << " " << (i) << endl;
-      cin >> a[i];
+      cout << "Enter number " << (i) << endl;
+      cin >> tempInt;
+      thisHeap.insert(tempInt);
+      cout << endl << "Size of array: " << thisHeap.getCount() << endl;
     }
-    build_maxheap(a,numofNum);
-    cout << "Max Heap" << endl;
-    for (i = 1; i <= numofNum; i++) {
-      cout << a[i] << endl;
+   
+  
+    while (thisHeap.getCount() > 0) {
+      cout << thisHeap.rmMax() << endl;
     }
   }
+  /*
   else if (input == "File") {
     while (std::getline(fileInput, line)) {
       numLines++;
     }
-    for (i = 1; i <= numLines; i++) {
+    for (i = 0; i <= numLines; i++) {
       
     }
   }
-}
 
+  */
+}
